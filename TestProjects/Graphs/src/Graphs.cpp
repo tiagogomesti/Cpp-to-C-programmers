f97241771efc4f0e569b5f79fc3f4ea10476c07e
Tiago Gomes <tiago.gomes.ext@altran.com>
2023-03-31 00:07:42 +0100

    Added code Node Class (Not tested)

--- TestProjects/Graphs/src/Graphs.cpp
+++ TestProjects/Graphs/src/Graphs.cpp
@@ -1,6 +1,88 @@
 #include "Graphs.h"
 
-int Graphs(void)
+//********************************************************************
+//********************************************************************
+//*                      N o d e s    C l a s s                      *
+//********************************************************************
+//********************************************************************
+
+CheckEdgeConnection Node::is_there_edge_to_this_node(uint32_t node_id)
 {
-    return 0;
+    CheckEdgeConnection check = {.is_connected = false, .array_index = 0};
+
+    for (uint32_t i = 0; i < this->edges.size(); i++)
+    {
+        if (this->edges[i].node->node_id == node_id)
+        {
+            check.is_connected = true;
+            check.array_index = i;
+            break;
+        }
+    }
+
+    return check;
 }
+
+Node::Node(uint32_t node_id)
+{
+    this->node_id = node_id;
+}
+
+Node::~Node(void)
+{
+}
+
+vector<Edge> Node::connected_nodes(void)
+{
+    return this->edges;
+}
+
+void Node::add_edge(Node &node, uint32_t weight)
+{
+    CheckEdgeConnection check = this->is_there_edge_to_this_node(node.node_id);
+
+    if (check.is_connected)
+    {
+        this->edges[check.array_index].weight = weight;
+    }
+    else
+    {
+        Edge new_edge = {.node = &node, .weight = weight};
+        this->edges.insert(edges.end(), new_edge);
+    }
+}
+
+void Node::change_weight(Node &node, uint32_t new_weight)
+{
+    CheckEdgeConnection check = this->is_there_edge_to_this_node(node.node_id);
+
+    if (!check.is_connected)
+    {
+        return;
+    }
+
+    this->edges[check.array_index].weight = new_weight;
+}
+
+void Node::remove_edge(Node &node)
+{
+    this->remove_edge(node.node_id);
+}
+
+void Node::remove_edge(uint32_t node_id)
+{
+    CheckEdgeConnection check = this->is_there_edge_to_this_node(node_id);
+
+    if (!check.is_connected)
+    {
+        return;
+    }
+
+    this->edges.erase(this->edges.begin() + check.array_index);
+}
+
+//********************************************************************
+//********************************************************************
+//*                      G r a p h    C l a s s                      *
+//********************************************************************
+//********************************************************************
--- TestProjects/Graphs/src/Graphs.h
+++ TestProjects/Graphs/src/Graphs.h
@@ -1,6 +1,57 @@
 #ifndef GRAPHS_H
 #define GRAPHS_H
 
-int Graphs(void);
+#include <cstdint>
+#include <vector>
 
-#endif
+using namespace std;
+
+class Node;
+
+typedef struct
+{
+    Node *node;
+    uint32_t weight;
+} Edge;
+
+typedef struct
+{
+    bool is_connected;
+    uint32_t array_index;
+} CheckEdgeConnection;
+
+class Node
+{
+public:
+    Node(uint32_t node_id);
+    ~Node(void);
+
+    vector<Edge> connected_nodes(void);
+    void add_edge(Node &node, uint32_t weight);
+    void change_weight(Node &node, uint32_t new_weight);
+    void remove_edge(Node &node);
+    void remove_edge(uint32_t node_id);
+
+private:
+    CheckEdgeConnection is_there_edge_to_this_node(uint32_t node_id);
+
+    uint32_t node_id;
+    vector<Edge> edges;
+};
+
+class Graph
+{
+public:
+    Graph(void);
+    ~Graph(void);
+
+    void add_node(uint32_t id);
+    void remove_node(uint32_t id);
+    void make_connection(uint32_t node_id_begin, uint32_t node_id_end, uint32_t weigh);
+    vector<Node> read_all_nodes(void);
+
+private:
+    vector<Node> nodes;
+};
+
+#endif /* GRAPHS_H */
